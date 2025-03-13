8ec9f6999028f706cd5b46b66a43b2a663c04d1c
KobusBooyens <kobusb@runninghill.co.za>
2025-03-13 18:50:52 +0200

    Added appointment form to add and delete tasks

--- src/app/appointment-list/appointment-list.component.ts
+++ src/app/appointment-list/appointment-list.component.ts
@@ -1,10 +1,39 @@
 import { Component } from '@angular/core';
+import { Appointment } from '../modules/appointment';
 
 @Component({
   selector: 'app-appointment-list',
   templateUrl: './appointment-list.component.html',
   styleUrls: ['./appointment-list.component.css']
 })
+
 export class AppointmentListComponent {
 
+  pageTitle: string = "My Appointments";
+  titleInput: string = "";
+  dateInput: Date = new Date();
+  appointments: Appointment[] = [];
+
+  addAppointment() {
+
+    if (this.titleInput.trim().length && this.dateInput) {
+      let newAppointment: Appointment = {
+        id: this.appointments.length + 1,
+        title: this.titleInput,
+        date: this.dateInput
+      };
+      this.appointments.push(newAppointment);
+
+      this.clearInput();
+    }
+  }
+
+  deleteAppointment(index: number) {
+    this.appointments.splice(index, 1);
+  }
+
+  clearInput() {
+    this.titleInput = "";
+    this.dateInput = new Date();
+  }
 }
